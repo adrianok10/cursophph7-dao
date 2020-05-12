86f371a7357680330acf5d332ce85e691e48bde9
Adriano k <adriano.klimiont@gmail.com>
2020-05-11 21:09:04 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,18 +2,31 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//carrega apenas um usuario para funcionar tem que tirar o static na linha doo getlist.
+//$root = new Usuario();
+//$root->loadbyId(5);
+//echo $root;
 
-$root->loadbyId(5);
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuário o login e a senha
+$usuario = new Usuario();
+$usuario->login("root", "xaxa");
+
+echo $usuario;
+
+
+
 
 //Select simples
-/*
-$sql = new Sql();
+//$sql = new Sql();
+//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+//echo json_encode($usuarios);
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-echo json_encode($usuarios);
-*/
  ?>
